f25f04f17213f69b7d0e812c6f4d847506eb276f
fernando-polo <fernandogmzmdo@gmail.com>
2025-02-24 18:21:05 -0600

    Creación y conexión a bd e index.html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import mysql.connector
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    conn =  mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="trolleo27",
+        database="db"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    cursor.close()
+    return render_template('index.html', students=students)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5001, debug=True)
